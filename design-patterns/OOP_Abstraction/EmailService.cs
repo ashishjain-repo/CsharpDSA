1781b0dbf03d73b0a2aa440b4e58466e6f30e1ed
Ashish Jain <ashishjain99@yahoo.com>
2024-12-02 23:58:02 -0700

    Working on ABstraction

---
+++ design-patterns/OOP_Abstraction/EmailService.cs
@@ -0,0 +1,22 @@
+public class EmailService
+{
+    public void SendEmail()
+    {
+        System.Console.WriteLine("Sending email...");
+    }
+
+    public void Connect()
+    {
+        System.Console.WriteLine("Connecting to email server...");
+    }
+
+    public void Authenticate()
+    {
+        System.Console.WriteLine("Authenticating...");
+    }
+
+    public void Disconnect()
+    {
+        System.Console.WriteLine("Disconnecting from email server...");
+    }
+}
---
+++ design-patterns/OOP_Abstraction/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        EmailService emailService = new EmailService();
+        emailService.Connect();
+        emailService.Authenticate();
+        emailService.SendEmail();
+        emailService.Disconnect();
+    }
+}
